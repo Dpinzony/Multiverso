03449ddf4be670cde89b2d8e1b4cbb12f84c30b0
Dpinzony <dpinzony@unal.edu.co>
2024-09-03 16:44:56 -0500

    Por algún motivo no funciona bien con el .jar

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,14 @@
+package javaapplication1;
+
+import GUI.Display;
+
+public class JavaApplication1 {
+
+    public static void main(String[] args) {
+        // Mostrar la ventana de Display
+        Display d = new Display();
+        d.setVisible(true);
+        d.setLocationRelativeTo(null);
+        
+    }
+}
